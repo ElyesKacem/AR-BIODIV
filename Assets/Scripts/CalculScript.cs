c195704833b43340720fab1b10b92cbf9c155688
elyes <elyeskacemeducation@gmail.com>
2023-11-14 21:41:11 +0100

    working on slider and eraser

---
+++ Assets/Scripts/CalculScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculScript : MonoBehaviour
+{
+    public static int archivment;
+    // Start is called before the first frame update
+    void Start()
+    {
+        archivment = 0;
+    }
+
+    public void incrementArchivment()
+    {
+        archivment += 1;
+    }
+}
